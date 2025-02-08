d7d663fa993f96b014906c2a83560a4edb11cb57
akash-nagar-95 <anagar@trueigtech.com>
2025-02-09 01:19:31 +0530

    fix : correct api fetching

---
+++ api_fetching/script.js
@@ -0,0 +1,18 @@
+const url = "https://cat-fact.herokuapp.com/facts";
+const button = document.querySelector("#btn");
+const factpara = document.querySelector("#factpara");
+
+const getData = async () => {
+    console.log("getting the data ..... ");
+    let response = await fetch(url);   // now this data is coming into the json format  so we need to convert it into understandable format (i.e., in the JS Object format).
+    console.log(response);
+
+    // so we'll use json() method to convert the json format into js object format : 
+    // using the json() method will also return second promise so we need to use "await" while conversion :
+    let data = await response.json();
+    console.log(data[0].text);
+    factpara.innerHTML = data[0].text;
+};
+
+
+button.addEventListener("click", getData);
